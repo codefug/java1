d452c309933e971a0464dc01b6cfe91fa834327a
codefug <86544979+codefug@users.noreply.github.com>
2023-03-11 19:24:55 +0900

    learning of base on java from installment to string

--- Data_and_operation/StringOperation.java
+++ Data_and_operation/StringOperation.java
@@ -4,8 +4,6 @@ public class StringOperation {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World".length()); // 11
-		System.out.println("Hello, [[[name]]] ... bye. ".replace("[[[name]]]", "duru"));
-
+		System.out.println("Hello, [[[name]]]. ".replace("[[[name]]]", "Lee seung hyun"));
 	}
-
 }
