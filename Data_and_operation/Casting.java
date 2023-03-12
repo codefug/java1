d5e1df01e5709c15fa4341bbf9276bb7c7114ba7
codefug <86544979+codefug@users.noreply.github.com>
2023-03-12 15:20:22 +0900

    Update Casting.java

--- Data_and_operation/Casting.java
+++ Data_and_operation/Casting.java
@@ -2,7 +2,7 @@
 public class Casting {
 
 	public static void main(String[] args) {
-		
+
 		double a = 1.1;
 		double b = 1;
 		double b2 = (double) 1;
@@ -14,11 +14,9 @@ public class Casting {
 		int e = (int) 1.1;
 		System.out.println(e);
 		
-		// 1 to String 
+		// 1 to String
 		String f = Integer.toString(1);
 		System.out.println(f.getClass());
-
-
 	}
 
 }
